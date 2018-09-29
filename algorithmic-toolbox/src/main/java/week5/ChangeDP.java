0c3552778d3081d6fb30ee182c8cd019e5477319
Jose Castillo <jccg1990@gmail.com>
2018-09-28 21:57:30 -0600

    adds week5 progress

--- algorithmic-toolbox/src/main/java/week5/ChangeDP.java
+++ algorithmic-toolbox/src/main/java/week5/ChangeDP.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ChangeDP {
     private static int getChange(int m) {
-        //write your code here
-        return m / 4;
+        ArrayList<Integer> coins = new ArrayList<>(Arrays.asList(1, 3, 4));
+        int[] minC = new int[m + 1];
+
+        minC[0] = 0;
+        for (int i = 1; i <= m; i++) {
+            minC[i] = Integer.MAX_VALUE;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int c : coins) {
+                if (i >= c) {
+                    int numCoin = minC[i - c] + 1;
+                    if (numCoin < minC[i]) minC[i] = numCoin;
+                }
+            }
+        }
+
+        return minC[m];
     }
 
     public static void main(String[] args) {
