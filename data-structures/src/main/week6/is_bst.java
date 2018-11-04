8792b79c054e50b561e21d8269edd64bc503ef84
Jose Castillo <jccg1990@gmail.com>
2018-11-04 15:54:33 -0700

    Adds week 6 and fixes weeks numbers

---
+++ data-structures/src/main/week6/is_bst.java
@@ -0,0 +1,96 @@
+package main.week6;
+
+import java.util.*;
+import java.io.*;
+
+public class is_bst {
+    class FastScanner {
+        StringTokenizer tok = new StringTokenizer("");
+        BufferedReader in;
+
+        FastScanner() {
+            in = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() throws IOException {
+            while (!tok.hasMoreElements())
+                tok = new StringTokenizer(in.readLine());
+            return tok.nextToken();
+        }
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public class IsBST {
+
+        class Node {
+            int key;
+            int left;
+            int right;
+
+            Node(int key, int left, int right) {
+                this.left = left;
+                this.right = right;
+                this.key = key;
+            }
+        }
+
+        int nodes;
+        Node[] tree;
+
+        void read() throws IOException {
+            FastScanner in = new FastScanner();
+            nodes = in.nextInt();
+            tree = new Node[nodes];
+            for (int i = 0; i < nodes; i++) {
+                tree[i] = new Node(in.nextInt(), in.nextInt(), in.nextInt());
+            }
+        }
+
+        boolean isBinarySearchTree() {
+          // traversing in order
+            if(tree.length <=  1) return true;
+
+            Stack<Node> s = new Stack<>();
+            Node curr = tree[0];
+            int prevValue = Integer.MIN_VALUE;
+
+            while(curr != null || !s.isEmpty()){
+                while (curr != null){
+                    s.push(curr);
+                    curr = curr.left == -1 ? null : tree[curr.left];
+                }
+
+                curr = s.pop();
+                if(curr.key <=  prevValue) return false;
+                else prevValue = curr.key;
+
+                curr = curr.right == -1 ? null : tree[curr.right];
+            }
+
+          return true;
+        }
+
+    }
+
+    static public void main(String[] args) throws IOException {
+        new Thread(null, new Runnable() {
+            public void run() {
+                try {
+                    new is_bst().run();
+                } catch (IOException e) {
+                }
+            }
+        }, "1", 1 << 26).start();
+    }
+    public void run() throws IOException {
+        IsBST tree = new IsBST();
+        tree.read();
+        if (tree.isBinarySearchTree()) {
+            System.out.println("CORRECT");
+        } else {
+            System.out.println("INCORRECT");
+        }
+    }
+}
