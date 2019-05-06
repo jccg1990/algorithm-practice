aebbc76895a8358cd35e22e2ee4d5605f6eb80f0
Jose Castillo <jccg1990@gmail.com>
2019-05-05 23:06:24 -0600

    completing excercises

--- AlgorithmsOnGraphs/src/main/java/week4/ShortestPaths.java
+++ AlgorithmsOnGraphs/src/main/java/week4/ShortestPaths.java
@@ -1,17 +1,86 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class ShortestPaths {
 
-    private static void shortestPaths(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost, int s, long[] distance, int[] reachable, int[] shortest) {
-      //write your code here
+    static ArrayList<Integer>[] adj;
+    static ArrayList<Integer>[] cost;
+    static long[] distance;
+    static int[] reachable;
+    static int[] shortest;
+    static HashSet<Integer> relaxedNodes;
+    static ArrayDeque<Integer> bfsQ;
+
+    private static void shortestPaths(int s, int vLength) {
+        relaxedNodes = new HashSet<>();
+        bfsQ = new ArrayDeque<>();
+        bellmanFord(s, vLength);
+        BFS();
+    }
+
+    static void BFS() {
+        while (!bfsQ.isEmpty()) {
+            int u = bfsQ.poll();
+            for (Integer v : adj[u]) {
+                if (shortest[v] == 1) {
+                    bfsQ.offer(v);
+                    shortest[v] = 0;
+                }
+            }
+        }
+    }
+
+    static void bellmanFord(int s, int vLength) {
+        distance[s] = 0;
+        reachable[s] = 1;
+
+        for (int i = 0; i <= vLength; i++) {
+            for (int from = 0; from < adj.length; from++) {
+                for (int j = 0; j < adj[from].size(); j++) {
+                    int to = adj[from].get(j);
+                    int weight = cost[from].get(j);
+
+                    boolean relaxed = relax(from, to, weight);
+
+                    if (relaxed && i == vLength) {
+                        if (!relaxedNodes.contains(from)) {
+                            relaxedNodes.add(from);
+                            bfsQ.add(from);
+                        }
+                        if (!relaxedNodes.contains(to)) {
+                            relaxedNodes.add(to);
+                            bfsQ.add(to);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    static boolean relax(int from, int to, int weight) {
+        long realDis = distance[from] == Long.MAX_VALUE ? Long.MAX_VALUE : distance[from] + weight;
+
+        if (distance[to] > realDis) {
+            distance[to] = realDis;
+            reachable[to] = 1;
+            return true;
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        FastScanner scanner = new FastScanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        ArrayList<Integer>[] cost = (ArrayList<Integer>[])new ArrayList[n];
+        adj = (ArrayList<Integer>[]) new ArrayList[n];
+        cost = (ArrayList<Integer>[]) new ArrayList[n];
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<Integer>();
             cost[i] = new ArrayList<Integer>();
@@ -25,15 +94,15 @@ public class ShortestPaths {
             cost[x - 1].add(w);
         }
         int s = scanner.nextInt() - 1;
-        long distance[] = new long[n];
-        int reachable[] = new int[n];
-        int shortest[] = new int[n];
+        distance = new long[n];
+        reachable = new int[n];
+        shortest = new int[n];
         for (int i = 0; i < n; i++) {
             distance[i] = Long.MAX_VALUE;
             reachable[i] = 0;
             shortest[i] = 1;
         }
-        shortestPaths(adj, cost, s, distance, reachable, shortest);
+        shortestPaths(s, m);
         for (int i = 0; i < n; i++) {
             if (reachable[i] == 0) {
                 System.out.println('*');
@@ -42,6 +111,35 @@ public class ShortestPaths {
             } else {
                 System.out.println(distance[i]);
             }
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new
+                        InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
         }
     }
 
--- AlgorithmsOnGraphs/src/main/java/week5/Clustering.java
+++ AlgorithmsOnGraphs/src/main/java/week5/Clustering.java
@@ -1,9 +1,93 @@
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Clustering {
+    static HashSet<Point> allPoints = new HashSet<>();
+    static HashSet<Point> set = new HashSet<>();
+    static PriorityQueue<Point> pq = new PriorityQueue<>();
+
     private static double clustering(int[] x, int[] y, int k) {
-        //write your code here
-        return -1.;
+        for (int i = 0; i < x.length; i++) {
+            Point p = new Point(x[i], y[i]);
+
+            if (i == 0) {
+                p.cost = 0.0;
+            }
+
+            addToDS(p);
+        }
+
+        int clusterNumber = set.size();
+
+        buildMST();
+
+        pq = new PriorityQueue<>(allPoints);
+
+        while (!pq.isEmpty()) {
+            Point v = pq.poll();
+
+            if (!find(v).equals(find(v.neighbour))) {
+                union(v, v.neighbour);
+                clusterNumber--;
+            }
+
+            if (clusterNumber == k) {
+                break;
+            }
+        }
+
+        return pq.peek() != null ? pq.peek().cost : 0;
+    }
+
+    private static void buildMST() {
+        while (!pq.isEmpty()) {
+            Point v = pq.poll();
+            set.remove(v);
+
+            for (Point z : set) {
+                double cost = calculateCost(v, z);
+                if (z.cost > cost) {
+                    z.cost = cost;
+                    z.neighbour = v;
+                    changePriority(pq, z);
+                }
+            }
+        }
+    }
+
+    private static void addToDS(Point p) {
+        allPoints.add(p);
+        set.add(p);
+        pq.add(p);
+    }
+
+    private static void changePriority(PriorityQueue<Point> q, Point z) {
+        q.remove(z);
+        q.add(z);
+    }
+
+    private static double calculateCost(Point v, Point z) {
+        return Math.sqrt(Math.pow(v.x - z.x, 2) + Math.pow(v.y - z.y, 2));
+    }
+
+    static Point find(Point p) {
+        if (p.parent.equals(p)) return p;
+
+        Point parent = find(p.parent);
+        p.parent = parent;
+
+        return parent;
+    }
+
+    static void union(Point a, Point b) {
+        if (a == null || b == null) return;
+
+        Point pa = find(a);
+        Point pb = find(b);
+
+        pa.parent = pb;
     }
 
     public static void main(String[] args) {
@@ -17,6 +101,40 @@ public class Clustering {
         }
         int k = scanner.nextInt();
         System.out.println(clustering(x, y, k));
+    }
+
+    static class Point implements Comparable<Point> {
+        int x;
+        int y;
+        Double cost = Double.MAX_VALUE;
+        Point parent;
+        Point neighbour;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.parent = this;
+            this.neighbour = this;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return x == point.x &&
+                    y == point.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return this.cost.compareTo(o.cost);
+        }
     }
 }
 
